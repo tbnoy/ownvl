9b4c6b9b84292cca15ea93364f866a7c31405c60
tzookb <tzookb@gmail.com>
2018-01-12 10:06:42 +0200

    fix tests

--- src/fetchFunction/tests/video_service/fields_decider_test.py
+++ src/fetchFunction/tests/video_service/fields_decider_test.py
@@ -4,23 +4,23 @@ from src.fetchFunction.video_service.fields_decider import fieldsDecider
 class FieldsDeciderTest(unittest.TestCase):
 
     def test_get_single(self):
-        fieldsIn = ['metadata']
+        fieldsIn = 'metadata'
         fieldsOut = fieldsDecider(fieldsIn)
-        self.assertEqual(fieldsIn, fieldsOut)
+        self.assertEqual(fieldsIn.split(','), fieldsOut)
 
     def test_get_several(self):
-        fieldsIn = ['metadata', 'urls', 'rays']
+        fieldsIn = 'metadata,urls,rays'
         fieldsOut = fieldsDecider(fieldsIn)
-        self.assertEqual(fieldsIn.sort(), fieldsOut.sort())
+        self.assertEqual(fieldsIn.split(',').sort(), fieldsOut.sort())
 
     def test_get_default(self):
-        fieldsIn = ['default']
+        fieldsIn = 'default'
         expectedDefault = ['urls', 'captions', 'modifiers', 'metadata']
         fieldsOut = fieldsDecider(fieldsIn)
         self.assertEqual(expectedDefault.sort(), fieldsOut.sort())
 
     def test_get_all(self):
-        fieldsIn = ['all']
+        fieldsIn = 'all'
         expectedAll = [
             'metadata',
             'links',
